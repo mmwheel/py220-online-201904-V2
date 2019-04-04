eb2b948b31d713b4bfec05a868ff789e0a62c674
Jeff Shabani <jeffrey.shabani@outlook.com>
2019-04-04 14:43:04 -0700

    calculator updates

--- students/jeff_shabani/lesson01/activity/calculator/calculator.py
+++ students/jeff_shabani/lesson01/activity/calculator/calculator.py
@@ -1,6 +1,14 @@
-from .exceptions import InsufficientOperands
+"""
+calculator class
+"""
+
+from exceptions import InsufficientOperands
+
 
 class Calculator(object):
+    """
+    main class
+    """
 
     def __init__(self, adder, subtracter, multiplier, divider):
         self.adder = adder
@@ -11,9 +19,15 @@ class Calculator(object):
         self.stack = []
 
     def enter_number(self, number):
+        """
+        method to enter a number into the calculator
+        """
         self.stack.insert(0, number)
 
     def _do_calc(self, operator):
+        """
+        method for running a calculation
+        """
         try:
             result = operator.calc(self.stack[0], self.stack[1])
         except IndexError:
@@ -23,13 +37,25 @@ class Calculator(object):
         return result
 
     def add(self):
+        """
+        addition method
+        """
         return self._do_calc(self.adder)
 
     def subtract(self):
+        """
+        subtraction method
+        """
         return self._do_calc(self.subtracter)
 
     def multiply(self):
+        """
+        multiplication method
+        """
         return self._do_calc(self.multiplier)
 
     def divide(self):
+        """
+        division method
+        """
         return self._do_calc(self.divider)
