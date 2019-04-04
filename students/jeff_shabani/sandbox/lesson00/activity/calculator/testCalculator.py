f293140c299f8c1588a283dc06728c8c8efd5dd7
admin <23247076+Rockwell70@users.noreply.github.com>
2019-04-04 06:02:48 -0700

    updated calculator tests

--- students/jeff_shabani/sandbox/lesson00/activity/calculator/testCalculator.py
+++ students/jeff_shabani/sandbox/lesson00/activity/calculator/testCalculator.py
@@ -64,3 +64,29 @@ class CalculatorTests(TestCase):
 
         self.subtractor.calc.assert_called_with(1,2)
 
+class ModuleTests(TestCase):
+
+    def test_module(self):
+
+        calculator = Calculator(Adder(), Subtracter(), Multiplier(), Divider())
+
+        calculator.enter_number(5)
+        calculator.enter_number(2)
+
+        calculator.multiply()
+
+        calculator.enter_number(46)
+
+        calculator.add()
+
+        calculator.enter_number(8)
+
+        calculator.divide()
+
+        calculator.enter_number(1)
+
+        result = calculator.subtract()
+
+        self.assertEqual(7, result)
+
+
